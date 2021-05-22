a77e8209fac21d7d8a871798000af2738353d4ee
rishabhshairy <rishusinha8@gmail.com>
2021-05-22 13:47:31 +0530

    Iniitial Commit

---
+++ src/com/arrays/gfg/TransitionPoint.java
@@ -0,0 +1,51 @@
+package com.arrays.gfg;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TransitionPoint {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while (t-- > 0) {
+			int n = sc.nextInt();
+			int a[] = new int[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = sc.nextInt();
+
+			}
+			int arr = transitionPoint(a, n);
+			System.out.print(arr);
+//			System.out.println();
+			sc.close();
+		}
+
+	}
+
+	static int transitionPoint(int arr[], int n) {
+		// code here
+		int r = n - 1;
+
+		int l = 0;
+
+		if (arr[l] == 1 && arr[r] == 1) {
+			return 0;
+		}
+
+		while (l <= r) {
+			int m = (l + r) / 2;
+
+			if (arr[m] == 0) {
+				l = m + 1;
+			} else if (arr[m] == 1) {
+				if (m == 0 || (m > 0 && arr[m - 1] == 0)) {
+					return m;
+				}
+				r = m - 1;
+			}
+
+		}
+		return -1;
+
+	}
+}
