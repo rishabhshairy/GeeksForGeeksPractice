e9f3e7df340e6971fcd0ba17a6a88a2f41c7588d
rishabhshairy <rishusinha8@gmail.com>
2021-05-22 20:42:35 +0530

    Frequency of number

---
+++ src/com/gfg/arrays/NumberFrequency.java
@@ -0,0 +1,34 @@
+package com.gfg.arrays;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class NumberFrequency {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while (t-- > 0) {
+			int n = sc.nextInt();
+			int a[] = new int[n];
+			for (int i = 0; i < a.length; i++) {
+				a[i] = sc.nextInt();
+			}
+			int k = sc.nextInt();
+			System.out.println(countFreq(a, k));
+		}
+		sc.close();
+	}
+
+	static int countFreq(int arr[], int k) {
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+		for (int i = 0; i < arr.length; i++) {
+			if (map.containsKey(arr[i])) {
+				map.put(arr[i], map.get(arr[i]) + 1);
+			} else {
+				map.put(arr[i], 1);
+			}
+		}
+		return map.get(k);
+	}
+}
