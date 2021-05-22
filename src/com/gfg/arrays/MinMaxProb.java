55ff72b1b75b7e850f982c14b15d5fd6817266b7
rishabhshairy <rishusinha8@gmail.com>
2021-05-22 14:56:17 +0530

    More practice

---
+++ src/com/gfg/arrays/MinMaxProb.java
@@ -0,0 +1,38 @@
+package com.gfg.arrays;
+
+import java.util.Scanner;
+
+public class MinMaxProb {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while (t-- > 0) {
+			int n = sc.nextInt();
+			int a[] = new int[n];
+			for (int i = 0; i < a.length; i++) {
+				a[i] = sc.nextInt();
+			}
+			System.out.println(getMax(a, n));
+			System.out.println(getMin(a, n));
+		}
+		sc.close();
+	}
+
+	static int getMin(int a[], int n) {
+		int res = a[0];
+
+		for (int i = 0; i < a.length; i++) {
+			res = Math.min(res, a[i]);
+		}
+		return res;
+	}
+
+	static int getMax(int a[], int n) {
+		int res = a[0];
+
+		for (int i = 0; i < a.length; i++) {
+			res = Math.max(res, a[i]);
+		}
+		return res;
+	}
+}
---
+++ src/com/gfg/arrays/ReverseArr.java
@@ -0,0 +1,42 @@
+package com.gfg.arrays;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ReverseArr {
+
+	public static void main(String args[]) throws IOException {
+		BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(read.readLine());
+
+		while (t-- > 0) {
+			String str = read.readLine();
+			System.out.println(reverseWord(str));
+		}
+	}
+
+	static String reverseWord(String s) {
+		int l = 0;
+		int r = s.length() - 1;
+		char[] res = s.toCharArray();
+		String ans = "";
+		for (int i = 0; i < res.length / 2; i++) {
+			if (res[l] == res[r]) {
+				l++;
+				r--;
+			} else {
+				char swapChar = res[l];
+				res[l] = res[r];
+				res[r] = swapChar;
+				l++;
+				r--;
+			}
+		}
+		for (int i = 0; i < res.length; i++) {
+			ans += res[i];
+		}
+
+		return ans;
+	}
+}
