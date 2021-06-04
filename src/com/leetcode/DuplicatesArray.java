f58ad7a7528d2839898aa38a5e994d7ff75679ea
rishabhshairy <rishusinha8@gmail.com>
2021-06-05 00:38:04 +0530

    Duplicates in Array
    leet code medium

---
+++ src/com/leetcode/DuplicatesArray.java
@@ -0,0 +1,24 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class DuplicatesArray {
+    public static void main(String[] args) {
+        int a[]={4,3,2,7,8,2,3,1};
+    }
+    static List<Integer> findDuplicates(int[] nums) {
+        List<Integer> answerList=new ArrayList<>();
+        Set<Integer> dupSet = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (dupSet.contains(nums[i])){
+                answerList.add(nums[i]);
+            } else {
+                dupSet.add(nums[i]);
+            }
+        }
+        return answerList;
+    }
+}
