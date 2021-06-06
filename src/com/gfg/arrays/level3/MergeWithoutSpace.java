74fe3f15f0ff80263d6334c5f9bcd3cf23512b9f
rishabhshairy <rishusinha8@gmail.com>
2021-06-06 14:56:11 +0530

    Fixed Merging Arrays

--- src/com/gfg/arrays/level3/MergeWithoutSpace.java
+++ src/com/gfg/arrays/level3/MergeWithoutSpace.java
@@ -40,7 +40,7 @@ public class MergeWithoutSpace {
 
     static void merge(int arr1[], int arr2[], int n, int m) {
         int i = 0, j = 0, k = n - 1;
-        while (i < n && j < m) {
+        while (i <= k && j < m) {
             if (arr1[i] < arr2[j]) {
                 i++;
             } else {
