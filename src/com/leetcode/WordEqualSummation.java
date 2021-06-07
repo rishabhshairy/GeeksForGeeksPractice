ad5f61972af1be8397759ea2ff88e339690373b4
rishabhshairy <rishusinha8@gmail.com>
2021-06-08 00:09:48 +0530

    Word Equation Sum
    LeetCode

---
+++ src/com/leetcode/WordEqualSummation.java
@@ -0,0 +1,34 @@
+package com.leetcode;
+
+public class WordEqualSummation {
+    public static void main(String[] args) {
+        String firstWord = "acb", secondWord = "cba", targetWord = "cdb";
+        System.out.println(isSumEqual(firstWord, secondWord, targetWord));
+    }
+
+    static boolean isSumEqual(String firstWord, String secondWord, String targetWord) {
+        int firstSum = 0;
+        int secondSum = 0;
+        int targetSum = 0;
+
+        for (char c :
+                firstWord.toCharArray()) {
+            int num = Math.abs(97 - (int) c);
+            firstSum = (firstSum * 10) + num;
+
+        }
+        for (char c :
+                secondWord.toCharArray()) {
+            int num = Math.abs(97 - (int) c);
+            secondSum = (secondSum * 10) + num;
+
+        }
+        for (char c :
+                targetWord.toCharArray()) {
+            int num = Math.abs(97 - (int) c);
+            targetSum = (targetSum * 10) + num;
+
+        }
+        return (firstSum + secondSum) == targetSum;
+    }
+}
