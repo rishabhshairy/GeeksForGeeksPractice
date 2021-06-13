246762a66d54d9176ec82f9330fa2bbf36b3f1a8
rishabhshairy <rishusinha8@gmail.com>
2021-06-13 17:18:03 +0530

    Fixes

--- src/com/leetcode/LongestCommonPrefix.java
+++ src/com/leetcode/LongestCommonPrefix.java
@@ -9,21 +9,21 @@ public class LongestCommonPrefix {
     }
 
     static String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0) {
-            return "";
-        }
-        Arrays.sort(strs);
-        String smallest = strs[0];
-        String largest = strs[strs.length - 1];
-        int i = 0;
-        for (i = 0; i < smallest.length(); ) {
-            if (smallest.charAt(i) == largest.charAt(i)) {
-                i++;
-            } else {
-                break;
+            if (strs == null || strs.length == 0) {
+                return "";
             }
-        }
+            Arrays.sort(strs);
+            String smallest = strs[0];
+            String largest = strs[strs.length - 1];
+            int i = 0;
+            for (i = 0; i < smallest.length(); ) {
+                if (smallest.charAt(i) == largest.charAt(i)) {
+                    i++;
+                } else {
+                    break;
+                }
+            }
 
-        return i == 0 ? "" : smallest.substring(0, i);
+            return i == 0 ? "" : smallest.substring(0, i);
     }
 }
