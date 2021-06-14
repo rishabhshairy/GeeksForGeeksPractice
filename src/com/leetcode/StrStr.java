9d81342e9f2b65e9a2c6aafeb602009209e8c897
rishabhshairy <rishusinha8@gmail.com>
2021-06-15 00:18:39 +0530

    Implement StrStr

---
+++ src/com/leetcode/StrStr.java
@@ -0,0 +1,29 @@
+package com.leetcode;
+
+public class StrStr {
+    public static void main(String[] args) {
+        String haystack = "hello";
+        String needle = "ll";
+        System.out.println(strStr(haystack, needle));
+    }
+
+    static int strStr(String haystack, String needle) {
+        if (needle.length() == 0) {
+            return 0;
+        }
+        if (haystack.length() == 0) {
+            return -1;
+        }
+
+        for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
+            int j = 0;
+            while (j < needle.length() && haystack.charAt(i + j) == needle.charAt(j)) {
+                j++;
+            }
+            if (j == needle.length()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
