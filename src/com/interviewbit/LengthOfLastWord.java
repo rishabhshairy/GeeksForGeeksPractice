1ce9d1b2b1c166b947477458745c784184a8472e
rishabhshairy <rishusinha8@gmail.com>
2021-06-15 00:19:57 +0530

    More String Questions

---
+++ src/com/interviewbit/LengthOfLastWord.java
@@ -0,0 +1,27 @@
+package com.interviewbit;
+
+public class LengthOfLastWord {
+    public static void main(String[] args) {
+        String s = "Hello World";
+        System.out.println(lengthOfLastWord(s));
+    }
+
+    static int lengthOfLastWord(final String A) {
+//        String[] allWord = A.split(" ");
+//        if (allWord.length > 0) {
+//            int n = allWord.length;
+//            return allWord[n - 1].length();
+//        }
+//        return 0;
+        int wordlength = 0;
+        for (char c :
+                A.toCharArray()) {
+            if (c != ' ') {
+                wordlength++;
+            } else {
+                wordlength = 0;
+            }
+        }
+        return wordlength;
+    }
+}
---
+++ src/com/interviewbit/ReverseString.java
@@ -0,0 +1,24 @@
+package com.interviewbit;
+
+import java.util.Arrays;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String s = "qxkpvo  f   w vdg t wqxy ln mbqmtwwbaegx   mskgtlenfnipsl bddjk znhksoewu zwh bd fqecoskmo";
+        System.out.println(solve(s));
+    }
+
+    static String solve(String A) {
+        String modify = A.trim();
+        String[] allWords = modify.split("\\s+");
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = allWords.length - 1; i >= 0; i--) {
+            if (allWords[i].length() > 0 && !allWords[i].equals(" ")) {
+                sb.append(allWords[i].trim());
+            }
+            sb.append(" ");
+        }
+        return sb.toString().trim();
+    }
+}
