7196603801d0fe0aade72a75a6640ec1a9c7c3b6
rishabhshairy <rishusinha8@gmail.com>
2021-06-16 22:43:27 +0530

    Top interview questions
    String type

---
+++ src/com/leetcode/ValidAnagram.java
@@ -0,0 +1,18 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+    public static void main(String[] args) {
+        String s = "anram", t = "nagaram";
+        System.out.println(isAnagram(s, t));
+    }
+
+    static boolean isAnagram(String s, String t) {
+        char[] sc = s.toCharArray();
+        char[] tc = t.toCharArray();
+        Arrays.sort(sc);
+        Arrays.sort(tc);
+        return Arrays.equals(sc, tc);
+    }
+}
