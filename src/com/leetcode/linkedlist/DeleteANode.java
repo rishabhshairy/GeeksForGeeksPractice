5e6e602476f6b911ada9340aba1c0516ce144348
rishabhshairy <rishusinha8@gmail.com>
2021-06-17 22:05:44 +0530

    Liinked List and Permutation

---
+++ src/com/leetcode/linkedlist/DeleteANode.java
@@ -0,0 +1,21 @@
+package com.leetcode.linkedlist;
+
+public class DeleteANode {
+    public static void main(String[] args) {
+
+    }
+
+    static void deleteNode(ListNode node) {
+
+    }
+}
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
