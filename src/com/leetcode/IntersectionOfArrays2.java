a72b7575a113b3024986dc165a188c5520e56f36
rishabhshairy <rishusinha8@gmail.com>
2021-06-19 01:52:50 +0530

    More Questions

---
+++ src/com/leetcode/IntersectionOfArrays2.java
@@ -0,0 +1,19 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class IntersectionOfArrays2 {
+    public static void main(String[] args) {
+        int[] num1 = {1, 2, 2, 1};
+        int[] num2 = {2, 2};
+    }
+
+    static int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        return new int[3];
+    }
+}
---
+++ src/com/leetcode/PowOfX.java
@@ -0,0 +1,29 @@
+package com.leetcode;
+
+public class PowOfX {
+
+    public static void main(String[] args) {
+        double x = 2.00000;
+        int n = -2147483648;
+        System.out.println(myPow(x, n));
+    }
+
+    static double myPow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n < 0) {
+            if (x == -1) {
+                return -1;
+            }
+            if (n == Integer.MIN_VALUE) {
+                n = n + 1;
+            }
+            n = -n;
+            x = 1 / x;
+        }
+        return n % 2 == 0 ? myPow(x * x, n / 2) : x * myPow(x * x, n / 2);
+
+    }
+}
+
