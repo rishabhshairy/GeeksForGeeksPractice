31cd19cd1f3f473b448788988607c08865cffb0e
rishabhshairy <rishusinha8@gmail.com>
2021-06-21 00:42:50 +0530

    Completed 100 questions
    Leetcode

---
+++ src/com/leetcode/DefangIP.java
@@ -0,0 +1,14 @@
+package com.leetcode;
+
+public class DefangIP {
+    public static void main(String[] args) {
+        String ipaddress = "1.1.1.1";
+        System.out.println(defangIPaddr(ipaddress));
+    }
+
+    static String defangIPaddr(String address) {
+        // StringBuilder sb = new StringBuilder(address);
+        String s = address.replaceAll("\\.", "[.]");
+        return s;
+    }
+}
---
+++ src/com/leetcode/MinStepsToMakeAnagram.java
@@ -0,0 +1,37 @@
+package com.leetcode;
+
+import java.util.HashMap;
+
+public class MinStepsToMakeAnagram {
+    public static void main(String[] args) {
+        String s = "abay";
+        String b = "babx";
+        System.out.println(minSteps(s, b));
+    }
+
+    static int minSteps(String s, String t) {
+        HashMap<Character, Integer> countOfS = new HashMap<>();
+
+        for (char c :
+                s.toCharArray()) {
+            if (countOfS.containsKey(c)) {
+                countOfS.put(c, countOfS.get(c) + 1);
+            } else {
+                countOfS.put(c, 1);
+            }
+        }
+
+        int answer = 0;
+        for (char c :
+                t.toCharArray()) {
+            if (countOfS.containsKey(c) && countOfS.get(c) > 0) {
+                System.out.println(countOfS.get(c) + " " + c);
+                countOfS.put(c, countOfS.get(c) - 1);
+            } else {
+                System.out.println(c);
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
