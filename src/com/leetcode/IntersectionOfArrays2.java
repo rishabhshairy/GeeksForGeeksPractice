e647e558ad1504a82f86fa9608dc9a5a6d6e66aa
rishabhshairy <rishusinha8@gmail.com>
2021-06-20 23:31:40 +0530

    Extra Questions

--- src/com/leetcode/IntersectionOfArrays2.java
+++ src/com/leetcode/IntersectionOfArrays2.java
@@ -1,19 +1,38 @@
 package com.leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class IntersectionOfArrays2 {
     public static void main(String[] args) {
         int[] num1 = {1, 2, 2, 1};
         int[] num2 = {2, 2};
+        intersect(num1, num2);
     }
 
     static int[] intersect(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        return new int[3];
+        HashMap<Integer, Integer> setMap = new HashMap<>();
+
+        for (Integer num :
+                nums1) {
+            if (setMap.containsKey(num)) {
+                setMap.put(num, setMap.get(num) + 1);
+            } else {
+                setMap.put(num, 1);
+            }
+
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        for (Integer num :
+                nums2) {
+            if (setMap.containsKey(num) && setMap.get(num) > 0) {
+                list.add(num);
+                setMap.put(num, setMap.get(num) - 1);
+            }
+        }
+        int[] ans = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            ans[i] = list.get(i);
+        }
+        return ans;
     }
 }
