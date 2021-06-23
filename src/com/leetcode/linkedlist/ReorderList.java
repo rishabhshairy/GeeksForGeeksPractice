f89f4f2d3e0d4a8c6590f8ba3e65aa7685bbc9be
rishabhshairy <rishusinha8@gmail.com>
2021-06-24 00:26:55 +0530

    Partition List

--- src/com/leetcode/linkedlist/ReorderList.java
+++ src/com/leetcode/linkedlist/ReorderList.java
@@ -100,11 +100,39 @@ public class ReorderList {
         }
         return l1;
     }
+
+    public ListNode partition(ListNode head, int x) {
+        ListNode less = new ListNode();
+        ListNode currLess = less;
+
+        ListNode more = new ListNode();
+        ListNode currMore = more;
+
+
+        ListNode temp = head;
+
+        while (temp != null) {
+            ListNode node = temp;
+            if (temp.val < x) {
+                currLess.next = node;
+                currLess = currLess.next;
+            } else {
+                currMore.next = node;
+                currMore = currMore.next;
+            }
+        }
+        currLess.next = more.next;
+        return less.next;
+
+    }
 }
 
 class ListNode {
     int val;
     ListNode next;
+
+    ListNode() {
+    }
 
     ListNode(int x) {
         val = x;
