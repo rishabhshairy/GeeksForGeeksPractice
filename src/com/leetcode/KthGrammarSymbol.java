690dfc0107738e34e831439728616553f5ecc21a
rishabhshairy <rishusinha8@gmail.com>
2021-06-24 00:27:12 +0530

    Kth Symbol Recursion

---
+++ src/com/leetcode/KthGrammarSymbol.java
@@ -0,0 +1,21 @@
+package com.leetcode;
+
+public class KthGrammarSymbol {
+    public static void main(String[] args) {
+        int n = 2, k = 2;
+        System.out.println(kthGrammar(n, k));
+    }
+
+    static int kthGrammar(int n, int k) {
+        int mid = (int) (Math.pow(2, n - 1) / 2);
+        if (n == 1 && k == 1) {
+            return 0;
+        }
+
+        if (k <= mid) {
+            return kthGrammar(n - 1, k);
+        } else {
+            return kthGrammar(n, k-mid) ^ 1;
+        }
+    }
+}
