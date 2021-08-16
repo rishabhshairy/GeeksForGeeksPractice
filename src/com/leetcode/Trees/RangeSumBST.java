58071f705805288aae2e405b4bce34bf71b0ee20
rishabhshairy <rishusinha8@gmail.com>
2021-08-16 22:06:56 +0530

    More Tree Questions

---
+++ src/com/leetcode/Trees/RangeSumBST.java
@@ -0,0 +1,32 @@
+package com.leetcode.Trees;
+
+import java.util.ArrayList;
+
+public class RangeSumBST {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        ArrayList<Integer> nums = new ArrayList<>();
+        int sum = 0;
+        solve(root, low, high, nums);
+        for (Integer num :
+                nums) {
+            sum += num;
+        }
+        return sum;
+    }
+
+    private void solve(TreeNode root, int low, int high, ArrayList<Integer> nums) {
+        if (root.left == null && root.right == null) {
+            if (root.val >= low && root.val <= high) {
+                nums.add(root.val);
+            }
+            return;
+        }
+        if (root.left != null) {
+            solve(root.left, low, high, nums);
+        }
+        if (root.right!=null){
+            solve(root.right, low, high, nums);
+        }
+
+    }
+}
