fac16fad3a71f571ad990fc1430707fe7ad2e2f3
rishabhshairy <rishusinha8@gmail.com>
2021-08-20 17:57:38 +0530

    Min Depth of Binary tree

---
+++ src/com/interviewbit/trees/MinDepth.java
@@ -0,0 +1,30 @@
+package com.interviewbit.trees;
+
+//Given a binary tree, find its minimum depth.
+//
+//The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+public class MinDepth {
+    public int minDepth(TreeNode A) {
+        if (A == null) {
+            return 0;
+        }
+        return solve(A);
+    }
+
+    private int solve(TreeNode a) {
+        if (a.right == null && a.left == null) {
+            return 1;
+        }
+        int leftCount = Integer.MAX_VALUE;
+        int rightCount = Integer.MAX_VALUE;
+
+        if (a.left != null) {
+            leftCount = solve(a.left) + 1;
+        }
+        if (a.right != null) {
+            rightCount = solve(a.right) + 1;
+        }
+        return Math.min(leftCount, rightCount);
+    }
+}
