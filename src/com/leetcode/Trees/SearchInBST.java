6c2300e9e468dde7f065efd555bfe2503af0b5bc
rishabhshairy <rishusinha8@gmail.com>
2021-08-25 00:11:38 +0530

    Average and search
    leetcode easy

---
+++ src/com/leetcode/Trees/SearchInBST.java
@@ -0,0 +1,28 @@
+package com.leetcode.Trees;
+
+public class SearchInBST {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val == val) {
+            return root;
+        }
+        return solve(root, val);
+    }
+
+    private TreeNode solve(TreeNode root, int val) {
+        if (root == null) {
+            return root;
+        }
+        if (root.val == val) {
+            return root;
+        }
+        if (val > root.val) {
+            return solve(root.right, val);
+        } else {
+            return solve(root.left, val);
+        }
+    }
+}
