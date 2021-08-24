498f81df8423bfe353045af3b56c947896ed15c1
rishabhshairy <rishusinha8@gmail.com>
2021-08-25 00:47:48 +0530

    All leaf at same level
    easy gfg

---
+++ src/com/gfg/trees/ToSumTree.java
@@ -0,0 +1,18 @@
+package com.gfg.trees;
+
+public class ToSumTree {
+    public void toSumTree(Node root) {
+        //add code here.
+        solve(root);
+    }
+
+    private int solve(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int prev = root.data;
+        root.data = solve(root.left) + solve(root.right);
+        return root.data + prev;
+    }
+
+}
