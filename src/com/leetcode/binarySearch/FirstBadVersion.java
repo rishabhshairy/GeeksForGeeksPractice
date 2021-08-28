7e99bf0c0b9f9f87bc04dd26a781151a942bf19e
rishabhshairy <rishusinha8@gmail.com>
2021-08-28 12:25:31 +0530

    Binary Search
    practise

---
+++ src/com/leetcode/binarySearch/FirstBadVersion.java
@@ -0,0 +1,32 @@
+package com.leetcode.binarySearch;
+
+public class FirstBadVersion {
+    public static void main(String[] args) {
+        int n = 5;
+        firstBadVersion(n);
+    }
+
+    public static int firstBadVersion(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int left = 1;
+        int right = n;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (!isBadVersion(mid)) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+    private static boolean isBadVersion(int n) {
+        return true;
+
+    }
+}
