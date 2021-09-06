d78ef5ab1314a4cee7d97d04dfc61dfa254de9ca
rishabhshairy <rishusinha8@gmail.com>
2021-09-07 01:12:31 +0530

    Fast track java
    Interview bit

---
+++ src/com/interviewbit/java/bigints/BigSumEx.java
@@ -0,0 +1,17 @@
+package com.interviewbit.java.bigints;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BigSumEx {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+
+        BigInteger a1 = new BigInteger(a);
+        BigInteger b1 = new BigInteger(b);
+
+        System.out.println(a1.add(b1));
+    }
+}
---
+++ src/com/interviewbit/java/strings/SBuilderEx.java
@@ -0,0 +1,30 @@
+package com.interviewbit.java.strings;
+
+import java.util.Scanner;
+
+public class SBuilderEx {
+    public static void main(String[] args) {
+        //Don't alter anything here.
+        Scanner inp = new Scanner(System.in);
+        String S = inp.nextLine();
+        int L = inp.nextInt();
+        inp.nextLine();
+        int R = inp.nextInt();
+        inp.nextLine();
+        inp.close();
+
+        System.out.println(solve(S, L, R));
+    }
+
+    //complete the function below
+
+    public static String solve(String s, int L, int R) {
+        String sub = s.substring(L, R + 1);
+        StringBuilder a = new StringBuilder(sub);
+        a.reverse();
+        String temp = a.toString();
+        StringBuilder b = new StringBuilder(s);
+        b.replace(L, R + 1, temp);
+        return b.toString();
+    }
+}
