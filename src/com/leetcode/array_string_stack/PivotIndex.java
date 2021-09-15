85a7d83412cb69a6e54de785fd88d4a30345a7d4
rishabhshairy <rishusinha8@gmail.com>
2021-09-16 02:07:48 +0530

    Array Strng Practice

---
+++ src/com/leetcode/array_string_stack/PivotIndex.java
@@ -0,0 +1,28 @@
+package com.leetcode.array_string_stack;
+
+import java.util.Arrays;
+
+public class PivotIndex {
+    public static void main(String[] args) {
+        int[] arr = {1, 7, 3, 6, 5, 6};
+        System.out.println(pivotIndex(arr));
+    }
+
+    public static int pivotIndex(int[] nums) {
+        int totalSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            totalSum += nums[i];
+        }
+        int currSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            currSum += nums[i];
+            if (currSum == totalSum) {
+                return i;
+            }
+            totalSum -= nums[i];
+        }
+        return -1;
+    }
+}
---
+++ src/com/leetcode/array_string_stack/RunningSum1d.java
@@ -0,0 +1,16 @@
+package com.leetcode.array_string_stack;
+
+public class RunningSum1d {
+    public int[] runningSum(int[] nums) {
+        int[] ans = new int[nums.length];
+        ans[0] = nums[0];
+        if (nums.length == 1) {
+            return ans;
+        }
+
+        for (int i = 1; i < nums.length; i++) {
+            ans[i] += ans[i - 1] + nums[i];
+        }
+        return ans;
+    }
+}
