f332a256b62436f3ef9b0818da5d0c7719bbb177
rishabhshairy <rishusinha8@gmail.com>
2021-10-02 23:41:29 +0530

    Stack and Dp

---
+++ src/com/leetcode/DP/JumpGame2.java
@@ -0,0 +1,32 @@
+package com.leetcode.DP;
+
+public class JumpGame2 {
+    public int jump(int[] nums) {
+        if (nums.length <= 0) {
+            return 0;
+        }
+        int jump = 0;
+        int max = nums[0];
+        int remainingSteps = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+
+            if (i == nums.length - 1) {
+                return jump;
+            }
+            max = Math.max(max, i + nums[i]);
+            remainingSteps--;
+            if (remainingSteps == 0) {
+                jump++;
+
+                if (i >= max) {
+                    return -1;
+                }
+
+                remainingSteps = max - i;
+            }
+            System.out.println("rem = " + remainingSteps + " i =" + i);
+        }
+        return jump;
+    }
+}
