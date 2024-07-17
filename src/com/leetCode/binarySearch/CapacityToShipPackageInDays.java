1ff0fa41aca84a3789d06aff0b853a5cb3aacf13
rishabhshairy <rishusinha8@gmail.com>
2024-07-18 00:20:32 +0530

    Capacity of Ship to package in days

--- src/com/leetCode/binarySearch/CapacityToShipPackageInDays.java
+++ src/com/leetCode/binarySearch/CapacityToShipPackageInDays.java
@@ -10,13 +10,35 @@ public class CapacityToShipPackageInDays {
 
     /**
      * This is binary search approach
+     *
      * @param weights
      * @param days
      * @return
      */
     public int shipWithinDays(int[] weights, int days) {
+        int maxWeight = Integer.MIN_VALUE;
+        int sumOfWeights = 0;
 
+        for (int i = 0; i < weights.length; i++) {
+            maxWeight = Math.max(maxWeight, weights[i]);
+            sumOfWeights += weights[i];
+        }
+
+        int low = maxWeight;
+        int high = sumOfWeights;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (findDays(weights, mid) <= days) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
     }
+
     /**
      * We Can ship load between maxWeight and sum of all weights
      *
@@ -42,6 +64,12 @@ public class CapacityToShipPackageInDays {
         return -1;
     }
 
+    /**
+     * Function to find number of days to send shipment
+     * @param weights
+     * @param currentWeightCapacity
+     * @return
+     */
     private static int findDays(int[] weights, int currentWeightCapacity) {
         int days = 1; // first day
         int load = 0;
