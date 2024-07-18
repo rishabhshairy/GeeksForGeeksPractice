fb8f20a2cd2f7b077a349d013d9359dd7d5069b6
rishabhshairy <rishusinha8@gmail.com>
2024-07-19 00:53:01 +0530

    Allocate Books, Painter Partition, Split Array
    All have same concept min(max)

---
+++ src/com/binaySearchConcepts/SplitArrayLargestSum.java
@@ -0,0 +1,54 @@
+package com.binaySearchConcepts;
+
+/**
+ * https://leetcode.com/problems/split-array-largest-sum/description/
+ * This problem is similar to Allocate books and Painter partition
+ */
+public class SplitArrayLargestSum {
+    public static void main(String[] args) {
+//        int[] arr = {7, 2, 5, 10, 8};
+        int[] arr = {10, 20, 30, 40};
+        int k = 2;
+        splitArray(arr, k);
+    }
+
+    public static int splitArray(int[] nums, int k) {
+        int max = Integer.MIN_VALUE;
+        int totalSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            max = Math.max(max, nums[i]);
+            totalSum += nums[i];
+        }
+
+        int low = max;
+        int high = totalSum;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+
+            if (checkSplit(nums, mid) > k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+    private static int checkSplit(int[] nums, int num) {
+        int currSplit = 1;
+        int currMaxSum = 0;
+
+        for (Integer currNum :
+                nums) {
+            if (currMaxSum + currNum <= num) {
+                currMaxSum += currNum;
+            } else {
+                currSplit++;
+                currMaxSum = currNum;
+            }
+        }
+        return currSplit;
+    }
+}
