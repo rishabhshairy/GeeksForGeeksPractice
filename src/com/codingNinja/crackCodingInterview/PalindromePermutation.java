0815292586af9eb0ba45aa6db96effe3e294a706
rishabhshairy <rishusinha8@gmail.com>
2024-07-29 23:35:32 +0530

    Cracking Code Interview Questions

---
+++ src/com/codingNinja/crackCodingInterview/PalindromePermutation.java
@@ -0,0 +1,27 @@
+package com.codingNinja.crackCodingInterview;
+
+/**
+ *
+ */
+public class PalindromePermutation {
+
+    public static void main(String[] args) {
+        String s = "aabbc";
+        palindromeString(s);
+    }
+
+    public static boolean palindromeString(String s) {
+        // Write your code here.
+        int[] count = new int[26];
+        for (char c :
+                s.toCharArray()) {
+            count[c - 'a']++;
+        }
+        int odd = 0;
+        for (int x :
+                count) {
+            odd += x & 1;
+        }
+        return odd < 2;
+    }
+}
