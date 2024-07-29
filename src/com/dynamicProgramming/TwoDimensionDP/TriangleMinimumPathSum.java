dc25defd023aa056016f5bc848213a4281c3a7e1
rishabhshairy <rishusinha8@gmail.com>
2024-07-29 23:34:16 +0530

    Triangle Path Min Sum Tabulation and Space Optimization

--- src/com/dynamicProgramming/TwoDimensionDP/TriangleMinimumPathSum.java
+++ src/com/dynamicProgramming/TwoDimensionDP/TriangleMinimumPathSum.java
@@ -11,7 +11,13 @@ public class TriangleMinimumPathSum {
 
     }
 
-    public int minimumTotal(List<List<Integer>> triangle) {
+    /**
+     * Recursion and Memoization approach
+     *
+     * @param triangle
+     * @return
+     */
+    public int minimumTotalRecursionMemoization(List<List<Integer>> triangle) {
         int[][] grid = triangle.stream().map(x -> x.stream().mapToInt(i -> i).toArray()).toArray(int[][]::new);
         int row = grid.length;
 
@@ -36,5 +42,53 @@ public class TriangleMinimumPathSum {
         int diagonal = grid[currRow][currCol] + solveRecurMemo(currRow + 1, currCol + 1, row, grid, dp);
 
         return dp[currRow][currCol] = Math.min(down, diagonal);
+    }
+
+    /**
+     * Tabulation Approach
+     *
+     * @param triangle
+     * @return
+     */
+    public int minimumTotalTabulation(List<List<Integer>> triangle) {
+        int[][] grid = triangle.stream().map(x -> x.stream().mapToInt(i -> i).toArray()).toArray(int[][]::new);
+        int row = grid.length;
+
+        int[][] dp = new int[row][row];
+        // base case
+        for (int j = 0; j < row; j++) {
+            dp[row - 1][j] = grid[row - 1][j];
+        }
+
+        // filling tabulation
+        for (int i = row - 2; i >= 0; i--) {
+            for (int j = i; j >= 0; j--) {
+                int down = grid[i][j] + dp[i + 1][j];
+                int diag = grid[i][j] + dp[i + 1][j + 1];
+                dp[i][j] = Math.min(down, diag);
+            }
+        }
+
+        return dp[0][0];
+    }
+
+    public int minimumTotalSpaceOptimised(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[] frontDp = new int[n];
+        int[] currDp = new int[n];
+        for (int j = 0; j < n; j++) {
+            frontDp[j] = triangle.get(n - 1).get(j);
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+
+            for (int j = i; j >= 0; j--) {
+                int down = triangle.get(i).get(j) + frontDp[j];
+                int diag = triangle.get(i).get(j) + frontDp[j+1];
+                currDp[j] = Math.min(down, diag);
+            }
+            frontDp = currDp.clone();
+        }
+        return frontDp[0];
     }
 }
