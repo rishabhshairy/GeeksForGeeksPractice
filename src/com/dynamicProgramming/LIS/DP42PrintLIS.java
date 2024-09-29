e7db0b6f897eec18f2bd76a80f0959a6f6c17ef4
rishabhshairy <rishusinha8@gmail.com>
2024-09-29 14:24:45 +0530

    DP 44 and DP45
    Largest Divisible and LongestStringChain

--- src/com/dynamicProgramming/LIS/DP42PrintLIS.java
+++ src/com/dynamicProgramming/LIS/DP42PrintLIS.java
@@ -6,9 +6,10 @@ import java.util.Collections;
 
 public class DP42PrintLIS {
     public static void main(String[] args) {
-//        int[] arr = {5, 4, 11, 1, 16, 8};
-        int[] arr = {1};
-        System.out.print(printLongestIncreasingSubsequence(arr.length, arr));
+        int[] arr = {5, 4, 11, 1, 16, 8};
+//        int[] arr = {1};
+//        System.out.print(printLongestIncreasingSubsequence(arr.length, arr));
+        System.out.print(longestIncreasingSubsequenceMinSpace(arr.length, arr));
     }
 
     public static ArrayList<Integer> printLongestIncreasingSubsequence(int n, int arr[]) {
@@ -48,7 +49,14 @@ public class DP42PrintLIS {
         return result;
     }
 
-    public int longestIncreasingSubsequenceMinSpace(int n, int[] arr) {
+    /**
+     * Printing uses same concept of Single DP array and hash array
+     *
+     * @param n
+     * @param arr
+     * @return
+     */
+    public static int longestIncreasingSubsequenceMinSpace(int n, int[] arr) {
         // Another way of finding LIS
         int[] dp = new int[n];
         Arrays.fill(dp, 1);
@@ -57,10 +65,12 @@ public class DP42PrintLIS {
 
         for (int index = 0; index < n; index++) {
             for (int prevIndex = 0; prevIndex < index; prevIndex++) {
+                System.out.println(arr[index] + " " + arr[prevIndex]);
                 if (arr[prevIndex] < arr[index]) {
                     dp[index] = Math.max(dp[index], 1 + dp[prevIndex]);
                 }
             }
+            System.out.println("loopended");
             maxi = Math.max(maxi, dp[index]);
         }
         return maxi;
