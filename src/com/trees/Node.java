5d082ace25a5464541643743ee3c55f371dd38e9
rishabhshairy <rishusinha8@gmail.com>
2024-10-01 21:57:58 +0530

    Fix Node util

--- src/com/trees/Node.java
+++ src/com/trees/Node.java
@@ -1,20 +1,14 @@
 package com.trees;
 
 public class Node {
-    int data;
-    Node left;
-    Node right;
+    public int data;
+    public Node left;
+    public Node right;
 
     public Node(int key) {
         data = key;
+        left = null;
+        right = null;
     }
 
-    public static void main(String[] args) {
-
-        Node root = new Node(1);
-        root.left = new Node(2);
-        root.right = new Node(3);
-        root.right.left = new Node(5);
-
-    }
 }
