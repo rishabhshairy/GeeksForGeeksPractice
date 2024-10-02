aec66c130c936eec56d56f70b702089b2d6480b4
rishabhshairy <rishusinha8@gmail.com>
2024-10-03 00:34:51 +0530

    Fix Leetcode symmetric tree code

--- src/com/leetCode/Trees/SymmetricTree.java
+++ src/com/leetCode/Trees/SymmetricTree.java
@@ -2,8 +2,21 @@ package com.leetCode.Trees;
 
 public class SymmetricTree {
     public boolean isSymmetric(TreeNode root) {
-        return traverse(root, root);
+        return root == null || isSymmetricHelp(root.left, root.right);
     }
+
+    private boolean isSymmetricHelp(TreeNode left, TreeNode right) {
+        if (left == null || right == null) {
+            return left == right;
+        }
+
+        if (left.val != right.val) {
+            return false;
+        }
+
+        return isSymmetricHelp(left.left, right.right) && isSymmetricHelp(left.right, right.left);
+    }
+
 
     private boolean traverse(TreeNode leftNode, TreeNode rightNode) {
         if (leftNode == null && rightNode == null) {
