67567ef1b39974a83d29673a3989b777ba07385c
rishabhshairy <rishusinha8@gmail.com>
2024-10-04 00:02:00 +0530

    LCA of Binary Tree

---
+++ src/com/trees/hard/LCAofBinaryTree.java
@@ -0,0 +1,29 @@
+package com.trees.hard;
+
+import com.trees.TreeNode;
+
+/**
+ * For leetcode
+ */
+public class LCAofBinaryTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left == null) {
+            return right;
+        } else if (right == null) {
+            return left;
+        } else {
+            return root;
+        }
+    }
+}
