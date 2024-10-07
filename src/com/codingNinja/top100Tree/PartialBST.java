c9c4fe0883534eca7940104aa2b98f669df331aa
rishabhshairy <rishusinha8@gmail.com>
2024-10-07 21:48:24 +0530

    Partial BST

---
+++ src/com/codingNinja/top100Tree/PartialBST.java
@@ -0,0 +1,38 @@
+package com.codingNinja.top100Tree;
+
+public class PartialBST {
+    /**
+     * Brute Force will be to write inorder and check if the array is sorted
+     *
+     * @param root
+     * @return
+     */
+    public static boolean validateBST(BinaryTreeNode<Integer> root) {
+        // Write your code here
+        return isPartialBst(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    /**
+     * Concept being used is
+     * for left subTree max will root
+     * and for right subtree min will root
+     *
+     * @param root
+     * @param minValue
+     * @param maxValue
+     * @return
+     */
+    private static boolean isPartialBst(BinaryTreeNode<Integer> root, int minValue, int maxValue) {
+        if (root == null) {
+            return true;
+        }
+
+        if (root.data >= minValue && root.data <= maxValue) {
+
+            boolean left = isPartialBst(root.left, minValue, root.data);
+            boolean right = isPartialBst(root.right, root.data, maxValue);
+            return left && right;
+        }
+        return false;
+    }
+}
