3d1bb4eb0d8440b78c81d6c56346cdac2803162f
rishabhshairy <rishusinha8@gmail.com>
2024-10-11 13:48:53 +0530

    Added optimal solution for range sum bst

--- src/com/leetCode/Trees/RangeSumBST.java
+++ src/com/leetCode/Trees/RangeSumBST.java
@@ -3,6 +3,23 @@ package com.leetCode.Trees;
 import java.util.ArrayList;
 
 public class RangeSumBST {
+    public int rangeSumBSTOptimal(TreeNode root, int low, int high) {
+        int[] sum = {0};
+        solveOptimal(root, low, high, sum);
+        return sum[0];
+    }
+
+    private void solveOptimal(TreeNode root, int low, int high, int[] sum) {
+        if (root == null) {
+            return;
+        }
+        solveOptimal(root.left, low, high, sum);
+        if (root.val >= low && root.val <= high) {
+            sum[0] += root.val;
+        }
+        solveOptimal(root.right, low, high, sum);
+    }
+
     public int rangeSumBST(TreeNode root, int low, int high) {
         ArrayList<Integer> nums = new ArrayList<>();
         int sum = 0;
@@ -24,7 +41,7 @@ public class RangeSumBST {
         if (root.left != null) {
             solve(root.left, low, high, nums);
         }
-        if (root.right!=null){
+        if (root.right != null) {
             solve(root.right, low, high, nums);
         }
 
